7384ba1d23251e77117a052cdb074d12070c096d
Kristian Trenskow <trenskow@me.com>
2021-02-13 10:53:52 +0100

    First version.

---
+++ README.md
@@ -0,0 +1,19 @@
+@trenskow/stack
+----
+
+Gets the current stack as an array.
+
+# Usage
+
+```javascript
+
+const stack = require('@trenskow/stack');
+
+const s = stack();
+```
+
+Returns an array of objects with the `function`, `file`, `line`, `character` keys.
+
+# License
+
+See LICENSE
---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports = module.exports = () => {
+	return (new Error()).stack
+		.split('\n')
+		.map(line => line.trim())
+		.slice(2)
+		.map((line) => {
+			return /^at ((.*?)(?: \[as (.*?)\])? \((.*?):([0-9]+):([0-9]+)\)|(.*?):([0-9]+):([0-9]+))$/.exec(line);
+		})
+		.filter(line => line)
+		.map((line) => {
+			return {
+				function: line[3] || line[2] || line[7],
+				file: line[4],
+				line: parseInt(line[5] || line[8]),
+				character: parseInt(line[6] || line[9])
+			};
+		});
+};
